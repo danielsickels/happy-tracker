367faed2f3fa02ffe7cafd7f3ce1eeb4e5677575
danielsickels <drsickels@gmail.com>
2025-02-07 19:16:09 -0500

    initialize nextjs app - happy tracker

---
+++ app/dashboard/page.js
@@ -0,0 +1,10 @@
+export const metadata = {
+    title: "Happy Tracker - Dashboard",
+  };
+
+export default function DashboardPage() {
+        return (
+            <div>Dashboard</div>
+        )
+
+}
