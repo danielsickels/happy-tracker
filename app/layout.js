a0b303c3c49f04138018653872d22ffb380c6f83
danielsickels <drsickels@gmail.com>
2025-02-08 00:18:16 -0500

    add components and styling

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,8 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Fugaz_One, Open_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const opensans = Open_Sans({ subsets: ["latin"] });
+const fugaz = Fugaz_One({ subsets: ["latin"], weight: ["400"] });
 
 export const metadata = {
   title: "Happy Tracker",
@@ -17,14 +10,39 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  const header = (
+    <header className="p-4 sm:p-8 flex items-center justify-between gap-4">
+      <h1 className={"text-base sm:text-lg textGradient " + fugaz.className}>
+        Happy Tracker
+      </h1>
+      <div className="flex items-center justify-center">PLACEHOLDER</div>
+    </header>
+  );
+
+  const footer = (
+    <footer className="p-4 sm:p-8 place-items-center">
+      <p
+        className={
+          "text-indigo-500 duration-200 hover:text-white hover:bg-indigo-500  " +
+          fugaz.className
+        }
+      >
+        Created with love
+      </p>
+    </footer>
+  );
+
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={
+          "w-full max-w-[1000px] mx-auto text-sm sm:text-base min-h-screen flex flex-col text-slate-800  " +
+          opensans.className
+        }
       >
-        <header>Header</header>
+        {header}
         {children}
-        <footer>Footer</footer>
+        {footer}
       </body>
     </html>
   );
--- components/Hero.js
+++ components/Hero.js
@@ -1,5 +1,30 @@
+import { Fugaz_One } from "next/font/google";
+import Button from "./Button";
+
 import React from "react";
+import Calendar from "./Calendar";
+const fugaz = Fugaz_One({ subsets: ["latin"], weight: ["400"] });
 
 export default function Hero() {
-  return <div>Hero</div>;
+  return (
+    <div className="py-4 md:py-10 flex flex-col gap-8 sm:gap-10">
+      <h1
+        className={
+          "text-5xl sm:text-6xl md:text-7xl text-center " + fugaz.className
+        }
+      >
+        <span className="textGradient">Happy Tracker</span> helps you track your{" "}
+        <span className="textGradient">daily</span> mood!
+      </h1>
+      <p className="text-lg sm:text-xl md:text-2xl text-center w-full mx-auto max-w-[600px]">
+        Record your mood to track how you feel
+        <span className="font-semibold"> every day of the year.</span>
+      </p>
+      <div className="grid grid-cols-2 gap-4 w-fit mx-auto">
+        <Button text="Sign Up" />
+        <Button text="Login" dark />
+      </div>
+      <Calendar />
+    </div>
+  );
 }
